8499cbab5eb637deb7c86542f57054a3b747db89
bajzello <krutny@gmail.com>
2017-12-24 16:01:28 +0100

    a

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+<<<<<<< HEAD
           <h2>Welcome to React v.0.0.1</h2>
 =======
           <h2>Welcome to React v.0.1</h2>
