ffaafe70a1100307bad6b65c2e2108cacc2b25a4
bajzello <krutny@gmail.com>
2017-12-25 00:35:14 +0100

    minor

--- react-ui/src/components/IndoorParticulateMatter.js
+++ react-ui/src/components/IndoorParticulateMatter.js
@@ -14,7 +14,7 @@ class OutdoorParticulateMatter extends FetchViaAPI {
     } else if (apiFetch.fulfilled) {
       return (
         <div>
-          <h1>Indoor Air Quality</h1>
+          <h1>Salon</h1>
           <h3>PM2.5: {apiFetch.value.FineParticulateMatter}</h3>
           <h3>PM10.0: {apiFetch.value.CoarseParticulateMatter}</h3>
           <h3>Humidity: {apiFetch.value.Humidity}</h3>
